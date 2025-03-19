4c4f6ebac15d07349040d013c2616465cf5476ae
Saketh <121789821+5aketh@users.noreply.github.com>
2025-03-20 03:33:00 +0530

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,17 +1,17 @@
 import Sidebar from './sidebar';
 import Chatbot from './chatbot';
-// import Weather from './weather';
-// import Forecast from './forecast';
+import Weather from './weather';
+import Forecast from './forecast';
 
 export default function Home() {
     return (
         <>
             <div style={{display: 'flex', flexDirection: 'row'}}>
                 <Sidebar />
-                {/* <div style={{paddingTop: "5vh", paddingLeft: "5vw"}}>
+                <div style={{paddingTop: "5vh", paddingLeft: "5vw"}}>
                     <Weather />
                     <Forecast />
-                </div> */}
+                </div>
                 <div style={{position: 'absolute', left: '68vw', top: '15vh'}}>
                     <Chatbot />
                 </div>
