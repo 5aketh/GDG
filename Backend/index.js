7be23dbb4a51e9d8301cd458ed20c02afc7367fe
Chinmay H <chinmay2806h@gmail.com>
2025-04-04 01:44:37 +0530

    express api limit

--- Backend/index.js
+++ Backend/index.js
@@ -6,11 +6,19 @@ import authRoutes from "./routes/auth.js";
 import sessionRoutes from "./routes/session.js";
 import chatbotRoutes from "./routes/chatbot.js"
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const apiLimiter = rateLimit({
+  windowMs: 5 * 60 * 60 * 24 * 1000,
+  max: 2,
+  message: "Too many requests, please try again later.",
+  headers: true,
+});
 
 app.use(cors({
   origin: [process.env.FRONTEND_LOCALHOST, process.env.FRONTEND_EXTERNAL_IP],  // Allow frontend origin
@@ -20,7 +28,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use("/auth", authRoutes);
-app.use("/chat", chatbotRoutes);
+app.use("/chat", apiLimiter, chatbotRoutes);
 // app.use("/shop", shopRoutes);
 
 app.get("/", (req,res)=>{
