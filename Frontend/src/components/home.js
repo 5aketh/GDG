7fffed55c364652cb0eae2bc6fbb58f667c21896
Saketh <121789821+5aketh@users.noreply.github.com>
2025-04-06 00:48:51 +0530

    Update home.js

--- Frontend/src/components/home.js
+++ Frontend/src/components/home.js
@@ -7,7 +7,9 @@ export default function Home() {
         <>
             <div style={{display: 'flex', flexDirection: 'row'}}>
                 <Sidebar />
-
+    
+                <a href='/login' style={{ color: '#fff', position: 'fixed', top:'5vh', left:'95vw' }}>Login</a>
+    
                 <div style={{ position: 'fixed', left: '20vw', top: '40vh', zIndex: 1000 }}>
                     <RotatingContent />
                 </div>
