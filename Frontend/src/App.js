86993a55cb0c4aec8c901d2e830f6256709dfd47
Saketh <121789821+5aketh@users.noreply.github.com>
2025-04-05 23:16:03 +0530

    Add files via upload

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,9 +2,11 @@ import Login from "./components/login";
 import Create from "./components/create";
 import Home from "./components/home";
 import Support from './components/support';
-import Shop from './components/shop';
+import Shop2Buy from './components/buy';
+import Shop2Sell from "./components/sell";
 import Settings from "./components/settings";
 import CropPrice from "./components/crop-price";
+import Weather from "./components/weather";
 import { Routes, Route } from "react-router-dom";
 import "./styles/login.css"
 import "./styles/sidebar.css"
@@ -30,13 +32,15 @@ function App(){
         <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@300;400;700&display=swap" rel="stylesheet" />
       </head>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/create" element={<Create />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/support" element={<Support />} />
-        <Route path="/shop/buy" element={<Shop />} />
+        <Route path="/shop/buy" element={<Shop2Buy />} />
+        <Route path="/shop/sell" element={<Shop2Sell />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/tools/market" element={<CropPrice />} />
+        <Route path="/tools/weather" element={<Weather />} />
       </Routes>
     </div>
   )
